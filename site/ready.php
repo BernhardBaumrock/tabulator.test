bafb49ff49856ca4758be1760c8d0881892edcda
Bernhard Baumrock <bernhard@baumrock.com>
2019-06-24 14:29:01 +0200

    multilang update

--- site/ready.php
+++ site/ready.php
@@ -1,1 +1,13 @@
 <?php namespace ProcessWire;
+/** @var Wire $this */
+
+// add global RockTabulator translation string
+$this->addHookAfter("InputfieldRockTabulator::getTranslations", function(HookEvent $event) {
+  $langs = $event->return;
+  $langs['foo'] = 'bar';
+  $event->return = $langs;
+});
+
+$this->addHookProperty("Language::locale", function($event) {
+  $event->return = 'xx-xx';
+});
